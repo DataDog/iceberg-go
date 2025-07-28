4f5130dbd51c37ac8c6438064bdfff9e59ed0c4d
Alex Normand <alex.normand@datadoghq.com>
2025-07-28 15:55:43 -0700

    Bootstrap fork with tooling to make it workable

---
+++ initialize-fork.sh
@@ -0,0 +1,24 @@
+#!/bin/zsh
+
+if [ ! -d "./go-cloud" ]; then
+    # Update the version if it gets updated upstream
+    git subtree add --prefix=go-cloud git@github.com:google/go-cloud.git v0.43.0
+    # Remove the go module definition to make go-cloud dev a sub-package
+    git rm go-cloud/go.*
+fi
+
+# Delete some workflows because that don't apply to the fork
+git rm .github/workflows/rc.yml
+git rm .github/workflows/license_check.yml
+git rm .github/workflows/go-release-docs.yml
+git rm .github/workflows/labeler.yml
+git rm .github/labeler.yml
+
+# Allow iceberg-go to compile by pointing the self-dependencies to this fork
+go mod edit -replace github.com/apache/iceberg-go=.
+
+# Replace all gocloud.dev import paths to the inlined subtree of the repo (effectivement vendoring gocloud.dev)
+go run github.com/sirkon/go-imports-rename@latest --save 'gocloud.dev => github.com/apache/iceberg-go/go-cloud'
+
+# Tidy up the dependencies
+go mod tidy
