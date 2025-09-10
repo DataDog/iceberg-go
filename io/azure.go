8bb12ab4230a152175d35af354c229cc1044db3e
Alessandro Nori <alessandro.nori@datadoghq.com>
2025-09-10 14:21:23 +0200

    reset to upstream main

--- io/azure.go
+++ io/azure.go
@@ -27,8 +27,8 @@ import (
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/container"
-	"gocloud.dev/blob"
-	"gocloud.dev/blob/azureblob"
+	"github.com/DataDog/iceberg-go/go-cloud/blob"
+	"github.com/DataDog/iceberg-go/go-cloud/blob/azureblob"
 )
 
 // Constants for Azure configuration options
